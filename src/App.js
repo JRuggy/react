9414008c1d4ddb4b3ac0491534548ef968ee8add
JastonRuggy <jastonruggy@icloud.com>
2022-12-14 14:13:32 +0300

    efcefef

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='kaka'>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          This is a trial for spring boot
         </p>
         <a
           className="App-link"
@@ -15,8 +16,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jaston Ruggy
         </a>
+
+        {/* This is a new Link */}
+        <a className='App-link kuku' href="https://toto.com" rel='noopener noreferrer' target="_blank">
+          Jarred
+        </a>
+
       </header>
     </div>
   );
